01021d58670fec7b189d06271770469b786ae8b7
roushni33 <lovelyroushni33@gmail.com>
2025-06-25 17:44:58 +0530

    added driver img in waiting for driver component

--- frontend/src/Components/WaitingForDriver.jsx
+++ frontend/src/Components/WaitingForDriver.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
 const WaitingForDriver = ({ setWaitingForDriverPanel }) => {
-  return (
-    <div>
+    return (
+        <div>
             <h5 onClick={() => {
                 setWaitingForDriverPanel(false)
             }} className='p-1 text-center w-[93%] absolute top-0 '><i className="ri-arrow-down-wide-line text-3xl text-gray-200 pt-14"></i></h5>
             <div className='flex items-center justify-between'>
-                <img className='h-12' src='https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1688398971/assets/29/fbb8b0-75b1-4e2a-8533-3a364e7042fa/original/UberSelect-White.png' />
+                              
+                <div className='relative flex items-center w-24 h-14'>
+                    <img
+                        className='h-14 w-14 rounded-full border-2 border-gray-300 object-cover z-20 shadow-lg'
+                        src='https://randomuser.me/api/portraits/men/32.jpg'
+                        alt='Driver'
+                    />
+                     <img
+                        className='h-12 w-12 rounded-full border-2 border-white object-cover absolute left-10 top-1 z-10'
+                        src='https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_538,w_956/v1688398971/assets/29/fbb8b0-75b1-4e2a-8533-3a364e7042fa/original/UberSelect-White.png'
+                        alt='Car'
+                    />
+                </div>
+               
+
                 <div className='text-right'>
                     <h2 className='text-lg font-medium'>Driver</h2>
                     <h4 className='text-xl font-semibold -mt-1 -mb-1'>MP04 AB 1234</h4>
@@ -15,9 +29,9 @@ const WaitingForDriver = ({ setWaitingForDriverPanel }) => {
                 </div>
             </div>
             <div className='flex flex-col gap-2 justify-between items-center'>
-               <div className='w-full mt-5'>
+                <div className='w-full mt-5'>
                     <div className='flex items-center gap-5 p-3 border-b-2'>
-                        <i className="ri-map-pin-user-fill"></i> 
+                        <i className="ri-map-pin-user-fill"></i>
                         <div className=''>
                             <h3 className='font-medium text-lg'>562/11-A</h3>
                             <p className='text-gray-600 text-sm -mt-1'>Kankariya Talab,Ahmedabad</p>
@@ -38,11 +52,11 @@ const WaitingForDriver = ({ setWaitingForDriverPanel }) => {
                         </div>
                     </div>
                 </div>
-               
+
             </div>
 
         </div>
-  )
+    )
 }
 
 export default WaitingForDriver
